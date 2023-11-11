164a167f5ab1c72bfef9d2ca5751ae991421cfa6
Adrian Botvinik <adrian.botvinik@gmail.com>
2023-11-10 16:16:23 -0800

    Redo for fixed converter, refactor

--- README.md
+++ README.md
@@ -1,8 +1,24 @@
 # Stationary Data Extractor
 
-Takes pickled data in the needed format, and removes all data that corresponds to segments where movement is less than a certain threshold, by default 10 m/s. Results placed in folder with same base name as input file, folder contains each continuous length of data that meets minimum velocity threshold.
+Takes pickled data in the needed format, and removes all data that corresponds to segments where movement is less than a certain threshold. Results placed in folder with same base name as input file, folder contains each continuous length of data that meets minimum velocity threshold.
+
+Change the threshold velocity using the global `THRESHOLD_VELOCITY` variable
 
 How to execute:
 ```
 python3 filter.py ./[Data file in .p format]
 ```
+
+Input data file is expected to have a dictionary with the following contents:
+
+`'name'`: Name of respective rosbag
+
+`'r'`: Rotation
+
+`'v'`: Velocity
+
+`'p'`: Position
+
+`'u'`: State vector ([$a_x$, $a_y$, $a_z$, $\omega_x$, $\omega_y$, $\omega_z$])
+
+`'t'`: Time
--- filter.py
+++ filter.py
@@ -4,6 +4,9 @@ import os
 import torch
 import numpy as np
 
+print('start')
+THRESHOLD_VELOCITY = 5
+
 filepath = sys.argv[1]
 name = filepath[len(filepath) - filepath[::-1].index('/'):
     len(filepath) - filepath[::-1].index('.') - 1]
@@ -12,78 +15,79 @@ if not os.path.exists(path): os.mkdir(path)
 
 data = pickle.load(open(filepath, "rb"))
 
-elems = data['u'].shape[0]
+elems = len(data['u'])
 
-init_ang_gt = data['ang_gt'].cpu().detach().numpy()
-init_p_gt = data['p_gt'].cpu().detach().numpy()
-init_t = data['t'].cpu().detach().numpy()
-init_v_gt = data['v_gt'].cpu().detach().numpy()
-init_u = data['u'].cpu().detach().numpy()
-init_quat_test = data['quat_test'].cpu().detach().numpy()
+init_r = data['r']
+init_p = data['p']
+init_t = data['t']
+init_v = data['v']
+init_u = data['u']
 init_t0 = data['t0']
 
 count = 0
 
-ang_gt = []
-p_gt = []
+r = []
+p = []
 t = []
-v_gt = []
+v = []
 u = []
-quat_test = []
 
-for i in range(0, elems):
-    print(i, end='\r')
+def reset():
+    global r, p, t, v, u
+    r = []
+    p = []
+    t = []
+    v = []
+    u = []
 
-    if len(ang_gt) == 0 and np.linalg.norm(init_v_gt[i]) < 10:
-        continue
-
-    if len(ang_gt) < 100 and np.linalg.norm(init_v_gt[i]) < 10:
-        ang_gt = []
-        p_gt = []
-        t = []
-        v_gt = []
-        u = []
-        quat_test = []
-        continue
-
-    if np.linalg.norm(init_v_gt[i]) < 10:
-        print('New list terminating')
-        t0 = init_t0 + t[0]
-        t = t - t[0]
-
-        d = {'name': name, 'ang_gt': ang_gt, 'p_gt': p_gt, 't': t.tolist(), 't0': t0, 'v_gt': v_gt, 'u': u, 'quat_test': quat_test}
-        print('Dictionary Made')
-
-        for key in d:
-            if isinstance(d[key], list):
-                print('Converting ' + key + ' to FloatTensor')
-                d[key] = torch.FloatTensor(d[key])
-            print(type(d[key]))
-
-        dmp = open(os.path.join('./' + name + '_filtered/', d["name"] + "_set_" + str(count) + ".p"), "wb")
-        pickle.dump(d, dmp)
-        count += 1
-        ang_gt = []
-        p_gt = []
-        t = []
-        v_gt = []
-        u = []
-        quat_test = []
-        continue
-
-    ang_gt.append(init_ang_gt[i])
-    p_gt.append(init_p_gt[i])
+def append(i):
+    global r, p, t, v, u
+    r.append(init_r[i])
+    p.append(init_p[i])
     t.append(init_t[i])
-    v_gt.append(init_v_gt[i])
+    v.append(init_v[i])
     u.append(init_u[i])
-    quat_test.append(init_quat_test[i])
 
-if len(ang_gt) > 100:
-    d = {'name': name, 'ang_gt': ang_gt, 'p_gt': p_gt, 't': t, 't0': t0, 'v_gt': v_gt, 'u': u, 'quat_test': quat_test}
+def dump():
+    global r, p, t, v, u, count
+    print('New list terminating')
+    t0 = init_t0 + t[0]
+    t = np.array(t) - t[0]
+    name_set = name + "_set_" + str(count)
 
-    for key in d:
-        if isinstance(d[key], list):
-            d[key] = torch.FloatTensor(d[key])
+    d = {'name': name_set, 'r': r, 'p': p, 't': t.tolist(), 't0': t0, 'v': v, 'u': u}
+    print('Dictionary Made')
 
-    dmp = open(os.path.join('./' + name + '_filtered/', d["name"] + "_set_" + str(count) + ".p"), "wb")
+    # for key in d:
+    #     if isinstance(d[key], list):
+    #         print('Converting ' + key + ' to FloatTensor')
+    #         d[key] = torch.FloatTensor(d[key])
+    #     print(type(d[key]))
+
+    dmp = open(os.path.join('./' + name + '_filtered/', d["name"] + ".p"), "wb")
     pickle.dump(d, dmp)
+    count += 1
+
+def filter():
+    for i in range(0, elems):
+        print(i, end='\r')
+
+        if len(r) == 0 and np.linalg.norm(init_v[i]) < THRESHOLD_VELOCITY:
+            continue
+
+        if len(r) < 100 and np.linalg.norm(init_v[i]) < THRESHOLD_VELOCITY:
+            reset()
+            continue
+
+        if np.linalg.norm(init_v[i]) < THRESHOLD_VELOCITY:
+            dump()
+            reset()
+            continue
+
+        append(i)
+
+    if len(r) > 100:
+        dump()
+    
+if __name__ == "__main__":
+    filter()
